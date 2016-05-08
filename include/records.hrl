83b8f623f4a1db6121cee44a9ce81aee3e4822c6
Stuart Thackray <stuart.thackray@gmail.com>
2016-05-08 19:58:06 +0200

    commit initial
    Intial Commit

---
+++ include/records.hrl
@@ -0,0 +1,7 @@
+
+
+-record(fa, {?ELEMENT_BASE(element_fa),
+        text=""                 :: text(),
+        body=""                 :: body,
+		fa 						:: text()		% Font awsume icon
+		}).
---
+++ src/element_fa.erl
@@ -0,0 +1,28 @@
+-module (element_fa).
+-include_lib ("nitrogen_core/include/wf.hrl").
+-include_lib ("../include/records.hrl").
+-export([
+    reflect/0,
+    render_element/1
+]). 
+ 
+reflect() -> record_info(fields, fa).
+
+
+-spec render_element(#fa{}) -> body().
+render_element(Record = #fa{text = Text}) ->
+    
+    UniversalAttributes = [
+    						{class, "fa fa-" ++ Record#fa.fa},
+							{"aria-hidden", "true"}
+						   ],
+	
+    case Body = Record#fa.body of
+        [] -> 
+            wf_tags:emit_tag(input, [
+                {value, Text}
+                | UniversalAttributes
+            ]);
+        _ ->
+            wf_tags:emit_tag(i, [Body, Text], UniversalAttributes)
+    end.
