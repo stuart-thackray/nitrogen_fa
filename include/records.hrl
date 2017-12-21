c32a997cccabea94956b10abb1ee9e7405992f2b
Jesse Gumm <gumm@sigma-star.com>
2017-12-21 13:06:11 -0600

    Add ifndef/endif pairs around include file
    (To prevent conflicts if include file is included more than once).

--- include/records.hrl
+++ include/records.hrl
@@ -1,7 +1,10 @@
-
+-ifndef(nitrogen_fa_include).
+-define(nitrogen_fa_include, 1).
 
 -record(fa, {?ELEMENT_BASE(element_fa),
         text=""                 :: text(),
         body=" "                 :: body,
 		fa 						:: text()		% Font awsume icon
 		}).
+
+-endif.
