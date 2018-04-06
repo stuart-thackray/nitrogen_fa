59677f024c7716d255764499cfecb9460f32d00d
Jesse Gumm <gumm@sigma-star.com>
2018-04-05 20:12:30 -0500

    Add size and fixed_width.  Support carrying class.
    Carrying the class from the passed in record allows the user to specify the
    class with #fa{class="something"}, thus allowing the generated HTML to retain
    the user-specified class.
    
    Additionally, if you wanted to change or replace the element in the DOM, the ID
    system Nitrogen employs ends up using the class attribute to create the ID.

--- include/records.hrl
+++ include/records.hrl
@@ -2,9 +2,11 @@
 -define(nitrogen_fa_include, 1).
 
 -record(fa, {?ELEMENT_BASE(element_fa),
-        text=""                 :: text(),
-        body=" "                 :: body,
-        fa                      :: text()       % Font awesome icon
+            text=""                 :: text(),
+            body=" "                 :: body,
+            fa                      :: text(), % Font awesome icon
+            size=""                 :: text(),
+            fw=false                :: boolean()
         }).
 
 -endif.
