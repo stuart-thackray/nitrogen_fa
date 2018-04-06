db8d1fd257958bda71895dab8813a761d5256af0
Jesse Gumm <gumm@sigma-star.com>
2018-04-05 20:22:43 -0500

    Add atom() support to fa and size attributes

--- include/records.hrl
+++ include/records.hrl
@@ -2,11 +2,11 @@
 -define(nitrogen_fa_include, 1).
 
 -record(fa, {?ELEMENT_BASE(element_fa),
-            text=""                 :: text(),
-            body=" "                 :: body,
-            fa                      :: text(), % Font awesome icon
-            size=""                 :: text(),
-            fw=false                :: boolean()
+             text=""                 :: text(),
+             body=" "                :: body(),
+             fa                      :: text() | atom(), % Font awesome icon
+             size=""                 :: text() | atom(),
+             fw=false                :: boolean()    %% fixed-width
         }).
 
 -endif.
